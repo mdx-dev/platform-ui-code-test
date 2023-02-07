4456a0570d9c34c8cab014ee91e7d8b0439cadf2
sipolyashhivangi <sipolyashivangi@gmail.com>
2023-02-07 14:28:08 -0500

    added code for selected and unselected items

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -31,6 +31,28 @@ export class ListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    try {
+      let data = sessionStorage.getItem('selectedItems');;
+      data = data ? JSON.parse(data) : [];
+      this.selectedProviders = this.selectedProviders.concat(data);
 
+      this.unselectedProviders = this.unselectedProviders.filter(item => this.selectedProviders.findIndex(subitem => subitem.id === item.id) === -1);
+    } catch (e) {}
+  }
+
+  select(item, index) {
+    console.log('hi', index)
+    this.unselectedProviders.splice(index, 1);
+    this.selectedProviders.push(item);
+    sessionStorage.setItem('selectedItems', JSON.stringify(this.selectedProviders));
+  }
+
+  deselect(item, index) {
+    console.log('bye', index)
+
+    this.selectedProviders.splice(index, 1);
+    this.unselectedProviders.push(item);
+    sessionStorage.setItem('selectedItems', JSON.stringify(this.selectedProviders));
+  }
 }
